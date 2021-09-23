5d450e92d84e8bdaad558bd1e6030a1a2c2da999
Nic-Ber <83864208+Nic-Ber@users.noreply.github.com>
2021-09-24 00:50:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from PIL import Image
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 import tensorflow as tf
-import cv2
+#import cv2
 import numpy as np
 #from pickle5 import load
 
