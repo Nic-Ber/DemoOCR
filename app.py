ecea6b4c73a9312a48372099ed69ac0fbbb0237f
Nic-Ber <83864208+Nic-Ber@users.noreply.github.com>
2021-10-06 23:11:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ if canvas_result.image_data is not None:
    
     with tf.GradientTape() as tape:
         last_conv_layer = model_3.get_layer('conv2d_4')
-        iterate = tf.keras.models.Model([model3.inputs], [model3.output, last_conv_layer.output])
+        iterate = tf.keras.models.Model([model_3.inputs], [model_3.output, last_conv_layer.output])
         model_out, last_conv_layer = iterate(img1)
         class_out = model_out[:, np.argmax(model_out[0])]
         grads = tape.gradient(class_out, last_conv_layer)
