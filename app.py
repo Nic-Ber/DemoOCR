00dcbcff12aafa8ad97995306efcfd7f7124d43c
Nic-Ber <83864208+Nic-Ber@users.noreply.github.com>
2021-10-12 00:44:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ if canvas_result.image_data is not None:
             last_conv_layer = model_3.get_layer('conv2d_4')
             iterate = tf.keras.models.Model([model_3.inputs], [model_3.output, last_conv_layer.output])
             model_out, last_conv_layer = iterate(img1)
-            st.write(model_out)
+            st.write(model_out.shape)
             text = ''
             for i in np.argmax(model_out, axis=2)[0,:]:
                 text += vocab[i]
