a1da4e56d25ecc78d099c0a4e55c9e90c1824b0f
Nic-Ber <83864208+Nic-Ber@users.noreply.github.com>
2021-11-11 15:52:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,8 @@ def confusion(logits):
     fig, ax = plt.subplots(figsize=(6,14))
     logits = tf.squeeze(tf.transpose(logits, (2, 1, 0)), axis=2)
     index = tf.argmax(logits, axis=0).numpy()
-    st.write(vocab[*[int(i) for i in index]])
+    lettres = [vocab(int(i)) for i in index]
+    st.write(lettres)
     #st.write(' - '.join(vocab[]))
     sns.heatmap(logits.numpy(), yticklabels=vocab, ax=ax, cbar=False) #cmap='flare_r'
     ax.set_title('Correlation matrix')
